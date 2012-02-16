1af5458cd6c8210efd8f6289a5a236f9cc937663
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2012-02-16 19:21:19 +0000

    Some extraction

--- src/main/java/com/phloc/webbasics/ui/bootstrap/BootstrapContentLayout.java
+++ src/main/java/com/phloc/webbasics/ui/bootstrap/BootstrapContentLayout.java
@@ -17,14 +17,8 @@
  */
 package com.phloc.webbasics.ui.bootstrap;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.phloc.html.hc.IHCNode;
 import com.phloc.html.hc.conversion.HCConversionSettings;
@@ -35,45 +29,17 @@ import com.phloc.html.hc.impl.AbstractWrappedHCNode;
 
 public class BootstrapContentLayout extends AbstractWrappedHCNode
 {
-  private static final class SpannedNode
-  {
-    private final EBootstrapSpan m_eSpan;
-    private final IHCNode m_aNode;
-
-    public SpannedNode (@Nonnull final EBootstrapSpan eSpan, @Nullable final IHCNode aNode)
-    {
-      m_eSpan = eSpan;
-      m_aNode = aNode;
-    }
-
-    @Nonnull
-    public IHCNode getAsSpannedNode ()
-    {
-      return m_eSpan.getAsNode (m_aNode);
-    }
-
-    @Nonnull
-    public int getSpanCount ()
-    {
-      return m_eSpan.getParts ();
-    }
-  }
-
-  private static final Logger s_aLogger = LoggerFactory.getLogger (BootstrapContentLayout.class);
-
   private HCDiv m_aContainer;
-  private final List <SpannedNode> m_aNodes = new ArrayList <SpannedNode> ();
+  private IHCNode m_aContent;
   private IHCNode m_aFooter;
 
   public BootstrapContentLayout ()
   {}
 
   @Nonnull
-  public BootstrapContentLayout addColumn (@Nonnull final EBootstrapSpan eSpan, @Nonnull final IHCNode aNode)
+  public BootstrapContentLayout setContent (@Nullable final IHCNode aContent)
   {
-    if (eSpan == null)
-      throw new NullPointerException ("span");
-    m_aNodes.add (new SpannedNode (eSpan, aNode));
+    m_aContent = aContent;
     return this;
   }
 
@@ -88,22 +54,8 @@ public class BootstrapContentLayout extends AbstractWrappedHCNode
   protected void prepareBeforeGetAsNode (@Nonnull final HCConversionSettings aConversionSettings)
   {
     m_aContainer = new HCDiv ().addClass (CBootstrapCSS.CONTAINER_FLUID);
-    if (!m_aNodes.isEmpty ())
-    {
-      final HCDiv aRow = m_aContainer.addAndReturnChild (new HCDiv ().addClass (CBootstrapCSS.ROW_FLUID));
-
-      int nSpanCount = 0;
-      for (final SpannedNode aSpannedNode : m_aNodes)
-      {
-        aRow.addChild (aSpannedNode.getAsSpannedNode ());
-        nSpanCount += aSpannedNode.getSpanCount ();
-      }
-      if (nSpanCount != 12)
-        s_aLogger.warn ("The overall spanning should be exactly 12 instead of " +
-                        nSpanCount +
-                        " for a consistent layout!");
-    }
-
+    if (m_aContent != null)
+      m_aContainer.addChild (m_aContent);
     if (m_aFooter != null)
       m_aContainer.addChildren (new HCHR (), new HCFooter (m_aFooter));
   }
---
+++ src/main/java/com/phloc/webbasics/ui/bootstrap/BootstrapContentRow.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.webbasics.ui.bootstrap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.html.hc.IHCNode;
+import com.phloc.html.hc.conversion.HCConversionSettings;
+import com.phloc.html.hc.html.HCDiv;
+import com.phloc.html.hc.impl.AbstractWrappedHCNode;
+
+public class BootstrapContentRow extends AbstractWrappedHCNode
+{
+  private static final class SpannedNode
+  {
+    private final EBootstrapSpan m_eSpan;
+    private final IHCNode m_aNode;
+
+    public SpannedNode (@Nonnull final EBootstrapSpan eSpan, @Nullable final IHCNode aNode)
+    {
+      m_eSpan = eSpan;
+      m_aNode = aNode;
+    }
+
+    @Nonnull
+    public IHCNode getAsSpannedNode ()
+    {
+      return m_eSpan.getAsNode (m_aNode);
+    }
+
+    @Nonnull
+    public int getSpanCount ()
+    {
+      return m_eSpan.getParts ();
+    }
+  }
+
+  private static final Logger s_aLogger = LoggerFactory.getLogger (BootstrapContentRow.class);
+
+  private HCDiv m_aRow;
+  private final List <SpannedNode> m_aNodes = new ArrayList <SpannedNode> ();
+
+  public BootstrapContentRow ()
+  {}
+
+  @Nonnull
+  public BootstrapContentRow addColumn (@Nonnull final EBootstrapSpan eSpan, @Nonnull final IHCNode aNode)
+  {
+    if (eSpan == null)
+      throw new NullPointerException ("span");
+    m_aNodes.add (new SpannedNode (eSpan, aNode));
+    return this;
+  }
+
+  @Override
+  protected void prepareBeforeGetAsNode (@Nonnull final HCConversionSettings aConversionSettings)
+  {
+    m_aRow = new HCDiv ().addClass (CBootstrapCSS.ROW_FLUID);
+
+    int nSpanCount = 0;
+    for (final SpannedNode aSpannedNode : m_aNodes)
+    {
+      m_aRow.addChild (aSpannedNode.getAsSpannedNode ());
+      nSpanCount += aSpannedNode.getSpanCount ();
+    }
+    if (nSpanCount != 12)
+      s_aLogger.warn ("The overall spanning should be exactly 12 instead of " +
+                      nSpanCount +
+                      " for a consistent layout!");
+  }
+
+  @Override
+  protected IHCNode getContainedHCNode ()
+  {
+    return m_aRow;
+  }
+}
