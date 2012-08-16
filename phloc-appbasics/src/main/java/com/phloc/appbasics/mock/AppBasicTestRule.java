32fb4b609aa7659785f2ca2031931f5ae13bc4bf
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2012-08-16 09:03:16 +0000

    Made storage path customizable

--- phloc-appbasics/src/main/java/com/phloc/appbasics/mock/AppBasicTestRule.java
+++ phloc-appbasics/src/main/java/com/phloc/appbasics/mock/AppBasicTestRule.java
@@ -1,25 +1,51 @@
 package com.phloc.appbasics.mock;
 
-import com.phloc.appbasics.app.ApplicationInitializer;
+import java.io.File;
+
+import javax.annotation.Nonnull;
+
 import com.phloc.appbasics.app.io.WebFileIO;
+import com.phloc.appbasics.app.io.WebIO;
+import com.phloc.appbasics.app.io.WebIOResourceProviderChain;
 import com.phloc.commons.idfactory.GlobalIDFactory;
 import com.phloc.commons.idfactory.MemoryIntIDFactory;
 import com.phloc.scopes.nonweb.mock.ScopeTestRule;
 
 public class AppBasicTestRule extends ScopeTestRule
 {
-  @Override
-  public void before () throws Throwable
+  private final File m_aStoragePath;
+
+  public AppBasicTestRule ()
   {
-    super.before ();
-    initAppBasics ();
+    this (ScopeTestRule.STORAGE_PATH);
   }
 
-  public static void initAppBasics ()
+  public AppBasicTestRule (@Nonnull final File aStoragePath)
+  {
+    if (aStoragePath == null)
+      throw new NullPointerException ("storagePath");
+    m_aStoragePath = aStoragePath;
+  }
+
+  @Nonnull
+  public File getStoragePath ()
+  {
+    return m_aStoragePath;
+  }
+
+  @Override
+  public void before ()
+  {
+    super.before ();
+    initAppBasics (m_aStoragePath);
+  }
+
+  public static void initAppBasics (@Nonnull final File aStoragePath)
   {
     // Init the base path once
     WebFileIO.resetBasePath ();
-    ApplicationInitializer.initIO (ScopeTestRule.STORAGE_PATH);
+    WebFileIO.initBasePath (aStoragePath);
+    WebIO.init (new WebIOResourceProviderChain (aStoragePath));
 
     // Init the IDs
     if (!GlobalIDFactory.hasPersistentIntIDFactory ())
