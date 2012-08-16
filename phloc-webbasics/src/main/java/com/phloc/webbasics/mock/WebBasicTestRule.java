18bad95fc876719be0c63e3f941f283dd02b879d
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2012-08-16 09:03:33 +0000

    Made storage path customizable

--- phloc-webbasics/src/main/java/com/phloc/webbasics/mock/WebBasicTestRule.java
+++ phloc-webbasics/src/main/java/com/phloc/webbasics/mock/WebBasicTestRule.java
@@ -1,6 +1,11 @@
 package com.phloc.webbasics.mock;
 
+import java.io.File;
+
+import javax.annotation.Nonnull;
+
 import com.phloc.appbasics.mock.AppBasicTestRule;
+import com.phloc.scopes.nonweb.mock.ScopeTestRule;
 import com.phloc.scopes.web.mock.MockHttpListener;
 import com.phloc.scopes.web.mock.MockServletRequestListener;
 import com.phloc.scopes.web.mock.WebScopeTestRule;
@@ -8,20 +13,40 @@ import com.phloc.webbasics.servlet.WebAppListener;
 
 public class WebBasicTestRule extends WebScopeTestRule
 {
-  @Override
-  public void before () throws Throwable
+  private final File m_aStoragePath;
+
+  public WebBasicTestRule ()
   {
-    super.before ();
-    initWebBasics ();
+    this (ScopeTestRule.STORAGE_PATH);
   }
 
-  public static void initWebBasics ()
+  public WebBasicTestRule (@Nonnull final File aStoragePath)
+  {
+    if (aStoragePath == null)
+      throw new NullPointerException ("storagePath");
+    m_aStoragePath = aStoragePath;
+  }
+
+  @Nonnull
+  public File getStoragePath ()
+  {
+    return m_aStoragePath;
+  }
+
+  @Override
+  public void before ()
+  {
+    super.before ();
+    initWebBasics (m_aStoragePath);
+  }
+
+  public static void initWebBasics (@Nonnull final File aStoragePath)
   {
     MockHttpListener.removeAllDefaultListeners ();
     MockHttpListener.addDefaultListener (new WebAppListener ());
     MockHttpListener.addDefaultListener (new MockServletRequestListener ());
     MockHttpListener.setToDefault ();
 
-    AppBasicTestRule.initAppBasics ();
+    AppBasicTestRule.initAppBasics (aStoragePath);
   }
 }
