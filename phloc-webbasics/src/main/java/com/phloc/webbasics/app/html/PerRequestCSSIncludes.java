27d834500ae89953c400b394a2e18e0e3f48168e
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2012-08-18 00:53:25 +0000

    Added per-request inclusion helper

---
+++ phloc-webbasics/src/main/java/com/phloc/webbasics/app/html/PerRequestCSSIncludes.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.webbasics.app.html;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.annotations.ReturnsImmutableObject;
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.html.resource.css.ICSSPathProvider;
+import com.phloc.scopes.web.domain.IRequestWebScope;
+import com.phloc.scopes.web.mgr.WebScopeManager;
+
+/**
+ * This class keeps track of all the CSS files that must be included for a
+ * single request, so that the controls are working properly.
+ * 
+ * @author philip
+ */
+public final class PerRequestCSSIncludes
+{
+  private static final String REQUEST_ATTR_CSSINCLUDE = PerRequestCSSIncludes.class.getName ();
+
+  private PerRequestCSSIncludes ()
+  {}
+
+  @Nullable
+  private static Set <ICSSPathProvider> _getPerRequestSet (final boolean bCreateIfNotExisting)
+  {
+    final IRequestWebScope aRequestScope = WebScopeManager.getRequestScope ();
+    Set <ICSSPathProvider> ret = aRequestScope.getCastedAttribute (REQUEST_ATTR_CSSINCLUDE);
+    if (ret == null && bCreateIfNotExisting)
+    {
+      ret = new LinkedHashSet <ICSSPathProvider> ();
+      aRequestScope.setAttribute (REQUEST_ATTR_CSSINCLUDE, ret);
+    }
+    return ret;
+  }
+
+  public static void registerCSSIncludeForThisRequest (@Nonnull final ICSSPathProvider aCSSPathProvider)
+  {
+    if (aCSSPathProvider == null)
+      throw new NullPointerException ("cssPathProvider");
+    _getPerRequestSet (true).add (aCSSPathProvider);
+  }
+
+  /**
+   * @return A non-<code>null</code> set with all CSS paths to be included in
+   *         this request. Order is ensured using LinkedHashSet.
+   */
+  @Nonnull
+  @ReturnsImmutableObject
+  public static Set <ICSSPathProvider> getAllRegisteredCSSIncludesForThisRequest ()
+  {
+    final Set <ICSSPathProvider> ret = _getPerRequestSet (false);
+    return ContainerHelper.makeUnmodifiableNotNull (ret);
+  }
+
+  /**
+   * @return <code>true</code> if at least a single CSS path has been registered
+   *         for this request only
+   */
+  public static boolean hasRegisteredCSSIncludesForThisRequest ()
+  {
+    return !ContainerHelper.isEmpty (_getPerRequestSet (false));
+  }
+}
---
+++ phloc-webbasics/src/main/java/com/phloc/webbasics/app/html/PerRequestJSIncludes.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.webbasics.app.html;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.annotations.ReturnsImmutableObject;
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.html.resource.js.IJSPathProvider;
+import com.phloc.scopes.web.domain.IRequestWebScope;
+import com.phloc.scopes.web.mgr.WebScopeManager;
+
+/**
+ * This class keeps track of all the JS files that must be included for a single
+ * request, so that the controls are working properly.
+ * 
+ * @author philip
+ */
+public final class PerRequestJSIncludes
+{
+  private static final String REQUEST_ATTR_JSINCLUDE = PerRequestJSIncludes.class.getName ();
+
+  private PerRequestJSIncludes ()
+  {}
+
+  @Nullable
+  private static Set <IJSPathProvider> _getPerRequestSet (final boolean bCreateIfNotExisting)
+  {
+    final IRequestWebScope aRequestScope = WebScopeManager.getRequestScope ();
+    Set <IJSPathProvider> ret = aRequestScope.getCastedAttribute (REQUEST_ATTR_JSINCLUDE);
+    if (ret == null && bCreateIfNotExisting)
+    {
+      ret = new LinkedHashSet <IJSPathProvider> ();
+      aRequestScope.setAttribute (REQUEST_ATTR_JSINCLUDE, ret);
+    }
+    return ret;
+  }
+
+  public static void registerJSIncludeForThisRequest (@Nonnull final IJSPathProvider aJSPathProvider)
+  {
+    if (aJSPathProvider == null)
+      throw new NullPointerException ("JSPathProvider");
+    _getPerRequestSet (true).add (aJSPathProvider);
+  }
+
+  /**
+   * @return A non-<code>null</code> set with all JS paths to be included in
+   *         this request. Order is ensured using LinkedHashSet.
+   */
+  @Nonnull
+  @ReturnsImmutableObject
+  public static Set <IJSPathProvider> getAllRegisteredJSIncludesForThisRequest ()
+  {
+    final Set <IJSPathProvider> ret = _getPerRequestSet (false);
+    return ContainerHelper.makeUnmodifiableNotNull (ret);
+  }
+
+  /**
+   * @return <code>true</code> if at least a single JS path has been registered
+   *         for this request only
+   */
+  public static boolean hasRegisteredJSIncludesForThisRequest ()
+  {
+    return !ContainerHelper.isEmpty (_getPerRequestSet (false));
+  }
+}
