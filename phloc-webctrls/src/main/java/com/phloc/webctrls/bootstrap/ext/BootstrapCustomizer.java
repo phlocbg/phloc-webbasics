b1044130df9ef06ada64a708ac4fe01d0ac62e5c
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2012-10-01 12:17:02 +0000

    Changed way how customization is handled

--- phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/ext/BootstrapCustomizer.java
+++ phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/ext/BootstrapCustomizer.java
@@ -3,7 +3,7 @@ package com.phloc.webctrls.bootstrap.ext;
 import javax.annotation.Nonnull;
 
 import com.phloc.html.EHTMLVersion;
-import com.phloc.html.hc.IHCElement;
+import com.phloc.html.hc.IHCBaseNode;
 import com.phloc.html.hc.IHCNodeWithChildren;
 import com.phloc.html.hc.customize.HCDefaultCustomizer;
 import com.phloc.webctrls.bootstrap.BootstrapDropDownMenu;
@@ -11,13 +11,13 @@ import com.phloc.webctrls.bootstrap.BootstrapDropDownMenu;
 public class BootstrapCustomizer extends HCDefaultCustomizer
 {
   @Override
-  public void customizeHCElement (@Nonnull final IHCNodeWithChildren <?> aParentElement,
-                                  @Nonnull final IHCElement <?> aElement,
-                                  @Nonnull final EHTMLVersion eHTMLVersion)
+  public void customizeNode (@Nonnull final IHCNodeWithChildren <?> aParentElement,
+                             @Nonnull final IHCBaseNode aNode,
+                             @Nonnull final EHTMLVersion eHTMLVersion)
   {
-    super.customizeHCElement (aParentElement, aElement, eHTMLVersion);
+    super.customizeNode (aParentElement, aNode, eHTMLVersion);
 
-    if (aElement instanceof BootstrapDropDownMenu)
+    if (aNode instanceof BootstrapDropDownMenu)
     {
       EBootstrapWorkarounds.IPAD_DROPDOWN_FIX.appendIfApplicable (aParentElement);
     }
