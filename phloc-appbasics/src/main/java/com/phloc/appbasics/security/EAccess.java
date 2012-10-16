9f872e99257d43bac4ec5d9aea5135453bf18ea4
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2012-10-16 09:55:53 +0000

    Updated API

--- phloc-appbasics/src/main/java/com/phloc/appbasics/security/EAccess.java
+++ phloc-appbasics/src/main/java/com/phloc/appbasics/security/EAccess.java
@@ -18,9 +18,11 @@
 package com.phloc.appbasics.security;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.phloc.commons.annotations.Nonempty;
 import com.phloc.commons.id.IHasID;
+import com.phloc.commons.lang.EnumHelper;
 
 /**
  * Generic access enum
@@ -57,5 +59,11 @@ public enum EAccess implements IHasID <String>
   public boolean isInherited ()
   {
     return this == INHERITED;
+  }
+
+  @Nullable
+  public static final EAccess getFromIDOrNull (@Nullable final String sID)
+  {
+    return EnumHelper.getFromIDOrNull (EAccess.class, sID);
   }
 }
