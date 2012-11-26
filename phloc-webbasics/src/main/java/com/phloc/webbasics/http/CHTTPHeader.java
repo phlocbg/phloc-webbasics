c902d7a3932a346c6353d59918ff5bd42ea32807
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2012-11-26 12:52:06 +0000

    Added CORS specific HTTP headers

--- phloc-webbasics/src/main/java/com/phloc/webbasics/http/CHTTPHeader.java
+++ phloc-webbasics/src/main/java/com/phloc/webbasics/http/CHTTPHeader.java
@@ -25,8 +25,7 @@ import javax.annotation.concurrent.Immutable;
  * @author philip
  */
 @Immutable
-public final class CHTTPHeader
-{
+public final class CHTTPHeader {
   public static final String ACCEPT = "Accept";
   public static final String ACCEPT_CHARSET = "Accept-Charset";
   public static final String ACCEPT_ENCODING = "Accept-Encoding";
@@ -78,6 +77,16 @@ public final class CHTTPHeader
   public static final String SET_COOKIE = "Set-Cookie";
   public static final String TRANSFER_ENCODING = "Transfer-Encoding";
 
-  private CHTTPHeader ()
-  {}
+  // CORS
+  public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+  public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+  public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+  public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+  public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+  public static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+  public static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+  public static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+  public static final String ORIGIN = "Origin";
+
+  private CHTTPHeader () {}
 }
