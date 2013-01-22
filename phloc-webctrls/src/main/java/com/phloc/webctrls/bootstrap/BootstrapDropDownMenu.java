814176983b73d7ae92e1bfc39b46776f49839afe
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-01-22 10:54:14 +0000

    Extended API

--- phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/BootstrapDropDownMenu.java
+++ phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/BootstrapDropDownMenu.java
@@ -21,6 +21,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.phloc.commons.url.ISimpleURL;
+import com.phloc.html.hc.IHCNode;
 import com.phloc.html.hc.html.HCA;
 import com.phloc.html.hc.html.HCLI;
 import com.phloc.html.hc.html.HCUL;
@@ -50,8 +51,25 @@ public class BootstrapDropDownMenu extends HCUL
   }
 
   @Nonnull
+  public BootstrapDropDownMenu addMenuItem (@Nonnull final ISimpleURL aURL,
+                                            @Nullable final IHCNode aLabel,
+                                            final boolean bActive)
+  {
+    final HCLI aLI = addAndReturnItem (new HCA (aURL).addChild (aLabel));
+    if (bActive)
+      aLI.addClass (CBootstrapCSS.ACTIVE);
+    return this;
+  }
+
+  @Nonnull
   public BootstrapDropDownMenu addActiveMenuItem (@Nonnull final ISimpleURL aURL, @Nullable final String sLabel)
   {
     return addMenuItem (aURL, sLabel, true);
+  }
+
+  @Nonnull
+  public BootstrapDropDownMenu addActiveMenuItem (@Nonnull final ISimpleURL aURL, @Nullable final IHCNode aLabel)
+  {
+    return addMenuItem (aURL, aLabel, true);
   }
 }
