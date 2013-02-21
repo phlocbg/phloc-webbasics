8e13abb90bdffb7d3e1b51450b35e81902ed8825
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-02-21 15:07:46 +0000

    In case somebody cares

---
+++ phloc-webctrls/src/main/java/com/phloc/webctrls/datatables/comparator/ComparatorTableFloat.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.webctrls.datatables.comparator;
+
+import java.util.Locale;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.annotations.OverrideOnDemand;
+import com.phloc.commons.compare.CompareUtils;
+import com.phloc.commons.format.IFormatter;
+import com.phloc.commons.locale.LocaleFormatter;
+
+public class ComparatorTableFloat extends AbstractComparatorTable
+{
+  protected final Locale m_aLocale;
+
+  public ComparatorTableFloat (@Nonnull final Locale aParseLocale)
+  {
+    this (null, aParseLocale);
+  }
+
+  public ComparatorTableFloat (@Nullable final IFormatter aFormatter, @Nonnull final Locale aParseLocale)
+  {
+    super (aFormatter);
+    if (aParseLocale == null)
+      throw new NullPointerException ("locale");
+    m_aLocale = aParseLocale;
+  }
+
+  @OverrideOnDemand
+  protected float getAsFloat (@Nonnull final String sCellText)
+  {
+    // Ensure that columns without text are sorted consistently compared to the
+    // ones with non-numeric content
+    if (sCellText.isEmpty ())
+      return Float.MIN_VALUE;
+    return LocaleFormatter.parseFloat (sCellText, m_aLocale, 0);
+  }
+
+  @Override
+  protected final int internalCompare (@Nonnull final String sText1, @Nonnull final String sText2)
+  {
+    final float f1 = getAsFloat (sText1);
+    final float f2 = getAsFloat (sText2);
+    return CompareUtils.compare (f1, f2);
+  }
+}
