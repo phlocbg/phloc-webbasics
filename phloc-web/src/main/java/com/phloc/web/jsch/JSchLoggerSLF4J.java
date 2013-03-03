7683fbc7e8787630ccd7d2f0c808f5581f7913a3
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-03-03 12:06:15 +0000

    Added some basic JSch stuff

---
+++ phloc-web/src/main/java/com/phloc/web/jsch/JSchLoggerSLF4J.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.web.jsch;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.commons.string.StringHelper;
+import com.phloc.commons.annotations.Nonempty;
+
+/**
+ * JSch logger implementation on top of SLF4J.
+ *
+ * @author philip
+ */
+@Immutable
+public final class JSchLoggerSLF4J implements com.jcraft.jsch.Logger
+{
+  private final Logger m_aLogger;
+
+  public JSchLoggerSLF4J (@Nonnull final Class <?> aClass)
+  {
+    this (aClass.getName ());
+  }
+
+  public JSchLoggerSLF4J (@Nonnull @Nonempty final String sLoggerName)
+  {
+    if (StringHelper.hasNoText (sLoggerName))
+      throw new IllegalArgumentException ("loggerName");
+    m_aLogger = LoggerFactory.getLogger (sLoggerName);
+  }
+
+  public boolean isEnabled (final int nLevel)
+  {
+    if (nLevel >= ERROR)
+      return m_aLogger.isErrorEnabled ();
+    if (nLevel >= WARN)
+      return m_aLogger.isWarnEnabled ();
+    if (nLevel >= INFO)
+      return m_aLogger.isInfoEnabled ();
+    return m_aLogger.isDebugEnabled ();
+  }
+
+  public void log (final int nLevel, final String sMessage)
+  {
+    if (nLevel >= ERROR)
+      m_aLogger.error (sMessage);
+    else
+      if (nLevel >= WARN)
+        m_aLogger.warn (sMessage);
+      else
+        if (nLevel >= INFO)
+          m_aLogger.info (sMessage);
+        else
+          m_aLogger.debug (sMessage);
+  }
+}
