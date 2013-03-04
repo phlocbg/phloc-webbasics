7f442110fc715c3d8729ba7cbc336f42523d0289
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-03-04 12:47:56 +0000

    Former-commit-id: 732c687dc5ab7bea373bbaf3296254b9a92f4313

--- phloc-appbasics/src/test/java/com/phloc/appbasics/bmx/BMXWriterTest.java
+++ phloc-appbasics/src/test/java/com/phloc/appbasics/bmx/BMXWriterTest.java
@@ -1,13 +1,11 @@
 package com.phloc.appbasics.bmx;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
 import org.junit.Test;
 
-import com.phloc.commons.io.file.FileOperations;
 import com.phloc.commons.io.file.SimpleFileIO;
 import com.phloc.commons.io.resource.ClassPathResource;
 import com.phloc.commons.microdom.IMicroDocument;
@@ -35,40 +33,12 @@ public final class BMXWriterTest
   @Test
   public void testStandardXML ()
   {
+    final StopWatch aSW = new StopWatch (true);
     final IMicroDocument aDoc = MicroReader.readMicroXML (new ClassPathResource ("bmx/standard.xml"));
+    System.out.println ("Reading via SAX took " + aSW.stopAndGetMillis () + "ms");
     final byte [] ret = new BMXWriter ().getAsBytes (aDoc);
     assertNotNull (ret);
     SimpleFileIO.writeFile (new File (ScopeTestRule.STORAGE_PATH, "standard.bmx"), ret);
     System.out.println (ret.length);
-  }
-
-  @Test
-  public void testSpeed ()
-  {
-    final File aSrcFile = ClassPathResource.getAsFile ("bmx/standard.xml");
-    final File aDestFile = new File (ScopeTestRule.STORAGE_PATH, "copy");
-
-    // Warmup
-    assertTrue (SimpleFileIO.copyFile (aSrcFile, aDestFile).isSuccess ());
-    FileOperations.deleteFile (aDestFile);
-    assertTrue (FileOperations.copyFile (aSrcFile, aDestFile).isSuccess ());
-    FileOperations.deleteFile (aDestFile);
-    StopWatch aSW;
-
-    aSW = new StopWatch (true);
-    for (int i = 0; i < 10; ++i)
-    {
-      assertTrue (FileOperations.copyFile (aSrcFile, aDestFile).isSuccess ());
-      FileOperations.deleteFile (aDestFile);
-    }
-    System.out.println ("FileOperations.copyFile: " + aSW.stopAndGetNanos ());
-
-    aSW = new StopWatch (true);
-    for (int i = 0; i < 10; ++i)
-    {
-      assertTrue (SimpleFileIO.copyFile (aSrcFile, aDestFile).isSuccess ());
-      FileOperations.deleteFile (aDestFile);
-    }
-    System.out.println ("SimpleFileIO.copyFile:   " + aSW.stopAndGetNanos ());
   }
 }
