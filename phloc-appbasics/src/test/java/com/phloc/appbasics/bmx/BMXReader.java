79744fd7455232ef692271b6e27701dedac2c7b3
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-03-06 10:08:52 +0000

    Former-commit-id: 91a17c6ecb393b89a988d29803688ae640dae01c

--- phloc-appbasics/src/test/java/com/phloc/appbasics/bmx/BMXReader.java
+++ phloc-appbasics/src/test/java/com/phloc/appbasics/bmx/BMXReader.java
@@ -196,4 +196,146 @@ public final class BMXReader
       StreamUtils.close (aISToUse);
     }
   }
+
+  // @Nullable
+  // public static IMicroNode readFromChannel (@Nonnull @WillClose final
+  // ReadableByteChannel aChannel)
+  // {
+  // if (aChannel == null)
+  // throw new NullPointerException ("channel");
+  //
+  // try
+  // {
+  // // Read version
+  // final byte [] aVersion = new byte [4];
+  // aChannel.readFully (aVersion);
+  // final String sVersion = new String (aVersion,
+  // CCharset.CHARSET_ISO_8859_1_OBJ);
+  // if (!sVersion.equals (CBMXIO.VERSION1))
+  // throw new BMXReadException ("This is not a BMX file!");
+  //
+  // // Read settings
+  // final int nSettings = aDIS.readInt ();
+  // final BMXSettings aSettings = BMXSettings.createFromStorageValue
+  // (nSettings);
+  //
+  // DataInputStream aContentDIS = aDIS;
+  // Inflater aInflater = null;
+  // InflaterInputStream aInflaterIS = null;
+  // if (false)
+  // {
+  // aInflater = new Inflater ();
+  // aInflaterIS = new InflaterInputStream (aDIS, aInflater);
+  // aContentDIS = new DataInputStream (aInflaterIS);
+  // }
+  //
+  // // Start iterating the main content
+  // IMicroNode aResultNode = null;
+  // final NonBlockingStack <IMicroNode> aNodeStack = new NonBlockingStack
+  // <IMicroNode> ();
+  // IMicroNode aLastNode = null;
+  // final BMXReaderStringTable aST = new BMXReaderStringTable (!aSettings.isSet
+  // (EBMXSetting.NO_STRINGTABLE));
+  //
+  // int nNodeType;
+  // while ((nNodeType = aContentDIS.readByte () & 0xff) != CBMXIO.NODETYPE_EOF)
+  // {
+  // IMicroNode aCreatedNode = null;
+  // switch (nNodeType)
+  // {
+  // case CBMXIO.NODETYPE_CDATA:
+  // aCreatedNode = new MicroCDATA (aST.get (aContentDIS.readInt ()));
+  // break;
+  // case CBMXIO.NODETYPE_COMMENT:
+  // aCreatedNode = new MicroComment (aST.get (aContentDIS.readInt ()));
+  // break;
+  // case CBMXIO.NODETYPE_CONTAINER:
+  // aCreatedNode = new MicroContainer ();
+  // break;
+  // case CBMXIO.NODETYPE_DOCUMENT:
+  // aCreatedNode = new MicroDocument ();
+  // break;
+  // case CBMXIO.NODETYPE_DOCUMENT_TYPE:
+  // {
+  // final String sQualifiedName = aST.get (aContentDIS.readInt ());
+  // final String sPublicID = aST.get (aContentDIS.readInt ());
+  // final String sSystemID = aST.get (aContentDIS.readInt ());
+  // aCreatedNode = new MicroDocumentType (sQualifiedName, sPublicID,
+  // sSystemID);
+  // break;
+  // }
+  // case CBMXIO.NODETYPE_ELEMENT:
+  // {
+  // final String sNamespaceURI = aST.get (aContentDIS.readInt ());
+  // final String sTagName = aST.get (aContentDIS.readInt ());
+  // final IMicroElement aElement = new MicroElement (sNamespaceURI, sTagName);
+  // final int nAttrCount = aContentDIS.readInt ();
+  // for (int i = 0; i < nAttrCount; ++i)
+  // {
+  // final String sAttrName = aST.get (aContentDIS.readInt ());
+  // final String sAttrValue = aST.get (aContentDIS.readInt ());
+  // aElement.setAttribute (sAttrName, sAttrValue);
+  // }
+  // aCreatedNode = aElement;
+  // break;
+  // }
+  // case CBMXIO.NODETYPE_ENTITY_REFERENCE:
+  // aCreatedNode = new MicroEntityReference (aST.get (aContentDIS.readInt ()));
+  // break;
+  // case CBMXIO.NODETYPE_PROCESSING_INSTRUCTION:
+  // {
+  // final String sTarget = aST.get (aContentDIS.readInt ());
+  // final String sData = aST.get (aContentDIS.readInt ());
+  // aCreatedNode = new MicroProcessingInstruction (sTarget, sData);
+  // break;
+  // }
+  // case CBMXIO.NODETYPE_TEXT:
+  // {
+  // final String sText = aST.get (aContentDIS.readInt ());
+  // final boolean bIgnorableWhitespace = aContentDIS.readBoolean ();
+  // aCreatedNode = new MicroText (sText, bIgnorableWhitespace);
+  // break;
+  // }
+  // case CBMXIO.NODETYPE_STRING:
+  // {
+  // final int nLength = aContentDIS.readInt ();
+  // final byte [] aString = new byte [nLength];
+  // aContentDIS.readFully (aString);
+  // aST.add (new String (aString, CBMXIO.ENCODING));
+  // break;
+  // }
+  // case CBMXIO.SPECIAL_CHILDREN_START:
+  // aNodeStack.push (aLastNode);
+  // break;
+  // case CBMXIO.SPECIAL_CHILDREN_END:
+  // aLastNode = aNodeStack.pop ();
+  // break;
+  // default:
+  // throw new BMXReadException ("Unsupported node type " + nNodeType);
+  // }
+  //
+  // if (aCreatedNode != null)
+  // {
+  // if (aResultNode == null)
+  // aResultNode = aCreatedNode;
+  // else
+  // aNodeStack.peek ().appendChild (aCreatedNode);
+  // aLastNode = aCreatedNode;
+  // }
+  // }
+  //
+  // if (aInflater != null)
+  // aInflater.end ();
+  //
+  // return aResultNode;
+  // }
+  // catch (final IOException ex)
+  // {
+  // throw new BMXReadException ("Failed to read from InputStream", ex);
+  // }
+  // finally
+  // {
+  // StreamUtils.close (aISToUse);
+  // }
+  // }
 }
