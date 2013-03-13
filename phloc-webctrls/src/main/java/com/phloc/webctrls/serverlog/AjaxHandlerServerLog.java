96bd28da725d12f0a1c4b82762cbb8a7d7982116
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-03-13 10:39:24 +0000

    Former-commit-id: bc86c51156d20abecc3bef552807e076759d130e

---
+++ phloc-webctrls/src/main/java/com/phloc/webctrls/serverlog/AjaxHandlerServerLog.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.webctrls.serverlog;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.commons.collections.attrs.MapBasedAttributeContainer;
+import com.phloc.commons.error.EErrorLevel;
+import com.phloc.commons.log.LogUtils;
+import com.phloc.commons.string.StringHelper;
+import com.phloc.webbasics.ajax.AbstractAjaxHandler;
+import com.phloc.webbasics.ajax.AjaxDefaultResponse;
+import com.phloc.webbasics.ajax.IAjaxResponse;
+import com.phloc.webscopes.domain.IRequestWebScopeWithoutResponse;
+
+public class AjaxHandlerServerLog extends AbstractAjaxHandler
+{
+  private static final EErrorLevel DEFAULT_SEVERITY = EErrorLevel.INFO;
+  private static final String PARAM_SEVERITY = "severity";
+  private static final String PARAM_MESSAGE = "message";
+  private static final String PARAM_KEY = "key";
+
+  @Nonnull
+  public static EErrorLevel getErrorLevelFromString (@Nullable final String sSeverity)
+  {
+    if (StringHelper.hasText (sSeverity))
+    {
+      if ("fatal".equalsIgnoreCase (sSeverity))
+        return EErrorLevel.FATAL_ERROR;
+      if ("error".equalsIgnoreCase (sSeverity))
+        return EErrorLevel.ERROR;
+      if ("warn".equalsIgnoreCase (sSeverity) || "warning".equalsIgnoreCase (sSeverity))
+        return EErrorLevel.WARN;
+      if ("info".equalsIgnoreCase (sSeverity))
+        return EErrorLevel.INFO;
+      if ("success".equalsIgnoreCase (sSeverity) ||
+          "debug".equalsIgnoreCase (sSeverity) ||
+          "trace".equalsIgnoreCase (sSeverity))
+        return EErrorLevel.SUCCESS;
+    }
+    return DEFAULT_SEVERITY;
+  }
+
+  @Override
+  @Nonnull
+  protected IAjaxResponse mainHandleRequest (@Nonnull final IRequestWebScopeWithoutResponse aRequestScope,
+                                             @Nonnull final MapBasedAttributeContainer aParams) throws Exception
+  {
+    final String sSeverity = aParams.getAttributeAsString (PARAM_SEVERITY);
+    final String sMessage = aParams.getAttributeAsString (PARAM_MESSAGE);
+    final String sKey = aParams.getAttributeAsString (PARAM_KEY);
+    final String sExpectedKey = ServerLogSessionKey.getGeneratedSessionKey ();
+    if (StringHelper.hasNoText (sMessage) || sExpectedKey == null || !sExpectedKey.equals (sKey))
+      return AjaxDefaultResponse.createError (null);
+
+    // Main logging
+    final EErrorLevel eSeverity = getErrorLevelFromString (sSeverity);
+    LogUtils.log (AjaxHandlerServerLog.class, eSeverity, sMessage);
+
+    // Convert the response to JSON
+    return AjaxDefaultResponse.createSuccess ();
+  }
+}
--- phloc-webctrls/src/main/java/com/phloc/webctrls/serverlog/ServerLogSessionKey.java
+++ phloc-webctrls/src/main/java/com/phloc/webctrls/serverlog/ServerLogSessionKey.java
@@ -9,9 +9,15 @@ import com.phloc.commons.string.StringHelper;
 import com.phloc.commons.string.ToStringGenerator;
 import com.phloc.webscopes.singleton.SessionWebSingleton;
 
+/**
+ * Session singleton to create unique keys for server logging. Each generated
+ * String has a length of 16.
+ * 
+ * @author philip
+ */
 public final class ServerLogSessionKey extends SessionWebSingleton
 {
-  private final String m_sKey;
+  private final String m_sGeneratedKey;
 
   @Deprecated
   @UsedViaReflection
@@ -19,7 +25,7 @@ public final class ServerLogSessionKey extends SessionWebSingleton
   {
     final byte [] aKey = new byte [8];
     VerySecureRandom.getInstance ().nextBytes (aKey);
-    m_sKey = StringHelper.getHexEncoded (aKey);
+    m_sGeneratedKey = StringHelper.getHexEncoded (aKey);
   }
 
   @Nonnull
@@ -28,22 +34,29 @@ public final class ServerLogSessionKey extends SessionWebSingleton
     return getSessionSingleton (ServerLogSessionKey.class);
   }
 
+  /**
+   * @return The generated session key and never <code>null</code>.
+   */
   @Nonnull
-  public String getKey ()
+  public String getGeneratedKey ()
   {
-    return m_sKey;
+    return m_sGeneratedKey;
   }
 
+  /**
+   * @return The generated session key or <code>null</code> if no
+   *         {@link ServerLogSessionKey} was created yet.
+   */
   @Nullable
-  public static String getSessionKey ()
+  public static String getGeneratedSessionKey ()
   {
     final ServerLogSessionKey aObj = getSingletonIfInstantiated (ServerLogSessionKey.class);
-    return aObj == null ? null : aObj.m_sKey;
+    return aObj == null ? null : aObj.m_sGeneratedKey;
   }
 
   @Override
   public String toString ()
   {
-    return new ToStringGenerator (this).append ("key", m_sKey).toString ();
+    return new ToStringGenerator (this).append ("generatedKey", m_sGeneratedKey).toString ();
   }
 }
