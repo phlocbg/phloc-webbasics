09e6bc789d3e5be0853bc012e41eaf4d8d71896b
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-04-03 05:59:09 +0000

    Customizable log level
    Former-commit-id: 44530d0d6054e7f40e9605b8f10caeef2547399b

--- phloc-web/src/main/java/com/phloc/web/smtp/listener/LoggingConnectionListener.java
+++ phloc-web/src/main/java/com/phloc/web/smtp/listener/LoggingConnectionListener.java
@@ -7,6 +7,9 @@ import javax.mail.event.ConnectionListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.phloc.commons.error.EErrorLevel;
+import com.phloc.commons.log.LogUtils;
+
 /**
  * An implementation of {@link ConnectionListener} that logs stuff to a logger.
  * 
@@ -16,18 +19,32 @@ public class LoggingConnectionListener implements ConnectionListener
 {
   private static final Logger s_aLogger = LoggerFactory.getLogger (LoggingConnectionListener.class);
 
+  private final EErrorLevel m_eErrorLevel;
+
+  public LoggingConnectionListener ()
+  {
+    this (EErrorLevel.INFO);
+  }
+
+  public LoggingConnectionListener (@Nonnull final EErrorLevel eErrorLevel)
+  {
+    if (eErrorLevel == null)
+      throw new NullPointerException ("ErrorLevel");
+    m_eErrorLevel = eErrorLevel;
+  }
+
   public void opened (@Nonnull final ConnectionEvent aEvent)
   {
-    s_aLogger.info ("Connected to SMTP server");
+    LogUtils.log (s_aLogger, m_eErrorLevel, "Connected to SMTP server");
   }
 
   public void disconnected (@Nonnull final ConnectionEvent aEvent)
   {
-    s_aLogger.info ("Disconnected to SMTP server");
+    LogUtils.log (s_aLogger, m_eErrorLevel, "Disconnected to SMTP server");
   }
 
   public void closed (@Nonnull final ConnectionEvent aEvent)
   {
-    s_aLogger.info ("Closed connection to SMTP server");
+    LogUtils.log (s_aLogger, m_eErrorLevel, "Closed connection to SMTP server");
   }
 }
--- phloc-web/src/main/java/com/phloc/web/smtp/listener/LoggingTransportListener.java
+++ phloc-web/src/main/java/com/phloc/web/smtp/listener/LoggingTransportListener.java
@@ -7,6 +7,9 @@ import javax.mail.event.TransportListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.phloc.commons.error.EErrorLevel;
+import com.phloc.commons.log.LogUtils;
+
 /**
  * An implementation of {@link TransportListener} that logs stuff to a logger.
  * 
@@ -16,18 +19,32 @@ public class LoggingTransportListener implements TransportListener
 {
   private static final Logger s_aLogger = LoggerFactory.getLogger (LoggingConnectionListener.class);
 
+  private final EErrorLevel m_eErrorLevel;
+
+  public LoggingTransportListener ()
+  {
+    this (EErrorLevel.INFO);
+  }
+
+  public LoggingTransportListener (@Nonnull final EErrorLevel eErrorLevel)
+  {
+    if (eErrorLevel == null)
+      throw new NullPointerException ("ErrorLevel");
+    m_eErrorLevel = eErrorLevel;
+  }
+
   public void messageDelivered (@Nonnull final TransportEvent aEvent)
   {
-    s_aLogger.info ("Message delivered");
+    LogUtils.log (s_aLogger, m_eErrorLevel, "Message delivered");
   }
 
   public void messageNotDelivered (@Nonnull final TransportEvent aEvent)
   {
-    s_aLogger.info ("Message not delivered");
+    LogUtils.log (s_aLogger, m_eErrorLevel, "Message not delivered");
   }
 
   public void messagePartiallyDelivered (@Nonnull final TransportEvent aEvent)
   {
-    s_aLogger.info ("Message partially delivered");
+    LogUtils.log (s_aLogger, m_eErrorLevel, "Message partially delivered");
   }
 }
