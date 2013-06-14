d9a5f5cb56dd4e256d03c591de2483618cb9bf1b
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-06-14 18:15:12 +0000

    Renamed and updated
    Former-commit-id: 5fb6b8b1b87b3b1a8767c51e43e799f2ee9193df

--- phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/ext/BootstrapPhlocTypeaheadScript.java
+++ phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/ext/BootstrapPhlocTypeaheadScript.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.phloc.webctrls.bootstrap.ext;
 
 import javax.annotation.Nonnegative;
@@ -6,7 +23,6 @@ import javax.annotation.Nullable;
 
 import com.phloc.commons.state.EChange;
 import com.phloc.commons.url.ISimpleURL;
-import com.phloc.html.hc.IHCNode;
 import com.phloc.html.hc.IHCNodeBuilder;
 import com.phloc.html.hc.html.HCScript;
 import com.phloc.html.js.builder.JSAnonymousFunction;
@@ -15,7 +31,7 @@ import com.phloc.html.js.builder.jquery.IJQuerySelector;
 import com.phloc.webbasics.app.html.PerRequestJSIncludes;
 import com.phloc.webctrls.bootstrap.EBootstrapJSPathProvider;
 
-public class BootstrapPhlocTypeahead implements IHCNodeBuilder
+public class BootstrapPhlocTypeaheadScript implements IHCNodeBuilder
 {
   private final IJQuerySelector m_aEditFieldSelector;
   private final JSAnonymousFunction m_aIDCallback;
@@ -23,9 +39,9 @@ public class BootstrapPhlocTypeahead implements IHCNodeBuilder
   private int m_nMinLength = 2;
   private int m_nMaxShowItems = 8;
 
-  public BootstrapPhlocTypeahead (@Nonnull final IJQuerySelector aEditFieldSelector,
-                                  @Nonnull final JSAnonymousFunction aIDCallback,
-                                  @Nonnull final ISimpleURL aAjaxInvocationURL)
+  public BootstrapPhlocTypeaheadScript (@Nonnull final IJQuerySelector aEditFieldSelector,
+                                        @Nonnull final JSAnonymousFunction aIDCallback,
+                                        @Nonnull final ISimpleURL aAjaxInvocationURL)
   {
     if (aEditFieldSelector == null)
       throw new NullPointerException ("EditFieldSelector");
@@ -74,7 +90,7 @@ public class BootstrapPhlocTypeahead implements IHCNodeBuilder
   }
 
   @Nullable
-  public IHCNode build ()
+  public HCScript build ()
   {
     PerRequestJSIncludes.registerJSIncludeForThisRequest (EBootstrapJSPathProvider.BOOTSTRAP_PHLOC_TYPEAHEAD);
     return new HCScript (new JSInvocation ("registerTypeaheadKeyValuePair").arg (m_aEditFieldSelector.getExpression ())
--- phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/ext/BootstrapTypeaheadEdit.java
+++ phloc-webctrls/src/main/java/com/phloc/webctrls/bootstrap/ext/BootstrapTypeaheadEdit.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2006-2013 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.phloc.webctrls.bootstrap.ext;
 
 import javax.annotation.Nonnull;
@@ -20,7 +37,8 @@ public class BootstrapTypeaheadEdit implements IHCNodeBuilder
 {
   private final HCEdit m_aEdit;
   private final RequestField m_aRFHidden;
-  private final ISimpleURL m_aAjaxInvocationURL;
+  private final String m_sHiddenFieldID;
+  private final BootstrapPhlocTypeaheadScript m_aScript;
 
   public BootstrapTypeaheadEdit (@Nonnull final RequestField aRFEdit,
                                  @Nonnull final RequestField aRFHidden,
@@ -34,8 +52,28 @@ public class BootstrapTypeaheadEdit implements IHCNodeBuilder
       throw new NullPointerException ("AjaxInvocationURL");
 
     m_aEdit = new HCEdit (aRFEdit).setDisableAutoComplete (true).setID (GlobalIDFactory.getNewStringID ());
+
     m_aRFHidden = aRFHidden;
-    m_aAjaxInvocationURL = aAjaxInvocationURL;
+    m_sHiddenFieldID = GlobalIDFactory.getNewStringID ();
+
+    final JSAnonymousFunction aIDCallback = new JSAnonymousFunction ();
+    final JSVar aID = aIDCallback.param ("id");
+    aIDCallback.body ().add (JQuery.idRef (m_sHiddenFieldID).val ().arg (aID));
+    m_aScript = new BootstrapPhlocTypeaheadScript (JQuerySelector.id (m_aEdit.getID ()),
+                                                   aIDCallback,
+                                                   aAjaxInvocationURL);
+  }
+
+  @Nonnull
+  public HCEdit getEdit ()
+  {
+    return m_aEdit;
+  }
+
+  @Nonnull
+  public BootstrapPhlocTypeaheadScript getScript ()
+  {
+    return m_aScript;
   }
 
   @Nullable
@@ -47,14 +85,10 @@ public class BootstrapTypeaheadEdit implements IHCNodeBuilder
     ret.addChild (m_aEdit);
 
     // HiddenField
-    final String sHiddenFieldID = GlobalIDFactory.getNewStringID ();
-    ret.addChild (new HCHiddenField (m_aRFHidden).setID (sHiddenFieldID));
+    ret.addChild (new HCHiddenField (m_aRFHidden).setID (m_sHiddenFieldID));
 
     // JS code
-    final JSAnonymousFunction aIDCallback = new JSAnonymousFunction ();
-    final JSVar aID = aIDCallback.param ("id");
-    aIDCallback.body ().add (JQuery.idRef (sHiddenFieldID).val ().arg (aID));
-    ret.addChild (new BootstrapPhlocTypeahead (JQuerySelector.id (m_aEdit.getID ()), aIDCallback, m_aAjaxInvocationURL));
+    ret.addChild (m_aScript);
 
     return ret;
   }
