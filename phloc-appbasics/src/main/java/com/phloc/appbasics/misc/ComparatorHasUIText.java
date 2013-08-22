e53aedf3ab665b50c47c32357241dde5ffeaa0d5
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-08-22 06:39:40 +0000

    Added additional constructors
    Former-commit-id: 616b5c79f040aa42fa1ec4ef415f1641d52ba040

--- phloc-appbasics/src/main/java/com/phloc/appbasics/misc/ComparatorHasUIText.java
+++ phloc-appbasics/src/main/java/com/phloc/appbasics/misc/ComparatorHasUIText.java
@@ -17,17 +17,184 @@
  */
 package com.phloc.appbasics.misc;
 
+import java.text.Collator;
+import java.util.Comparator;
 import java.util.Locale;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.phloc.commons.compare.AbstractCollationComparator;
+import com.phloc.commons.compare.ESortOrder;
 
+/**
+ * Special comparator that compares based on a UI text
+ * 
+ * @author Philip Helger
+ */
 public class ComparatorHasUIText extends AbstractCollationComparator <IHasUIText>
 {
-  public ComparatorHasUIText (@Nonnull final Locale aSortLocale)
+  /**
+   * Comparator with default locale {@link Collator} and default sort order.
+   */
+  public ComparatorHasUIText ()
+  {
+    super ();
+  }
+
+  /**
+   * Comparator with default locale {@link Collator} and default sort order and
+   * a nested comparator.
+   * 
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorHasUIText (@Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aNestedComparator);
+  }
+
+  /**
+   * Comparator with default locale {@link Collator}.
+   * 
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   */
+  public ComparatorHasUIText (@Nonnull final ESortOrder eSortOrder)
+  {
+    super (eSortOrder);
+  }
+
+  /**
+   * Comparator with default locale {@link Collator} and a nested comparator.
+   * 
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorHasUIText (@Nonnull final ESortOrder eSortOrder,
+                              @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (eSortOrder, aNestedComparator);
+  }
+
+  /**
+   * Comparator with default sort order and specified sort locale.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   */
+  public ComparatorHasUIText (@Nullable final Locale aSortLocale)
   {
     super (aSortLocale);
+  }
+
+  /**
+   * Comparator with default sort order but special locale and a nested
+   * comparator.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorHasUIText (@Nullable final Locale aSortLocale,
+                              @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aSortLocale, aNestedComparator);
+  }
+
+  /**
+   * Constructor with locale and sort order.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   */
+  public ComparatorHasUIText (@Nullable final Locale aSortLocale, @Nonnull final ESortOrder eSortOrder)
+  {
+    super (aSortLocale, eSortOrder);
+  }
+
+  /**
+   * Constructor with locale and sort order and a nested comparator.
+   * 
+   * @param aSortLocale
+   *        The locale to use. May be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorHasUIText (@Nullable final Locale aSortLocale,
+                              @Nonnull final ESortOrder eSortOrder,
+                              @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aSortLocale, eSortOrder, aNestedComparator);
+  }
+
+  /**
+   * Constructor with {@link Collator} using the default sort order
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   */
+  public ComparatorHasUIText (@Nonnull final Collator aCollator)
+  {
+    super (aCollator);
+  }
+
+  /**
+   * Constructor with {@link Collator} using the default sort order and a nested
+   * comparator.
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorHasUIText (@Nonnull final Collator aCollator,
+                              @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aCollator, aNestedComparator);
+  }
+
+  /**
+   * Constructor with {@link Collator} and sort order.
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   */
+  public ComparatorHasUIText (@Nonnull final Collator aCollator, @Nonnull final ESortOrder eSortOrder)
+  {
+    super (aCollator, eSortOrder);
+  }
+
+  /**
+   * Constructor with {@link Collator} and sort order and a nested comparator.
+   * 
+   * @param aCollator
+   *        The {@link Collator} to use. May not be <code>null</code>.
+   * @param eSortOrder
+   *        The sort order to use. May not be <code>null</code>.
+   * @param aNestedComparator
+   *        The nested comparator to be invoked, when the main comparison
+   *        resulted in 0.
+   */
+  public ComparatorHasUIText (@Nonnull final Collator aCollator,
+                              @Nonnull final ESortOrder eSortOrder,
+                              @Nullable final Comparator <? super Object> aNestedComparator)
+  {
+    super (aCollator, eSortOrder, aNestedComparator);
   }
 
   @Override
