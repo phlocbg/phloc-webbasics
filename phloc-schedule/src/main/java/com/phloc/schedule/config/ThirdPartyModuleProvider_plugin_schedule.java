65f782df9341d052dbad4ae46a9fd2ecc6278b7c
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-08-22 07:50:11 +0000

    Updated to Quartz 2.2.0
    Former-commit-id: 393875625fc5dc1e3a9517959dc635218a818b53

--- phloc-schedule/src/main/java/com/phloc/schedule/config/ThirdPartyModuleProvider_plugin_schedule.java
+++ phloc-schedule/src/main/java/com/phloc/schedule/config/ThirdPartyModuleProvider_plugin_schedule.java
@@ -38,7 +38,7 @@ public final class ThirdPartyModuleProvider_plugin_schedule implements IThirdPar
   private static final IThirdPartyModule QUARTZ = new ThirdPartyModule ("Quartz",
                                                                         "Terracotta",
                                                                         ELicense.APACHE2,
-                                                                        new Version (2, 1, 7),
+                                                                        new Version (2, 2, 0),
                                                                         "http://www.quartz-scheduler.org/");
 
   @Nullable
