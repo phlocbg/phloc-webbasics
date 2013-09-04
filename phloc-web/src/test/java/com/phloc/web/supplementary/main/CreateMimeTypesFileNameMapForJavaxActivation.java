1deb4b654f90d3d335d6ab40712f1a29b6d3b26e
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-09-04 06:03:29 +0000

    Moved tools
    Former-commit-id: cd23bf836286071a6e0a7107dfd0df7afca65b6a

--- phloc-web/src/test/java/com/phloc/web/supplementary/main/CreateMimeTypesFileNameMapForJavaxActivation.java
+++ phloc-web/src/test/java/com/phloc/web/supplementary/main/CreateMimeTypesFileNameMapForJavaxActivation.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.phloc.tools;
+package com.phloc.web.supplementary.main;
 
 import java.io.File;
 import java.io.PrintWriter;
