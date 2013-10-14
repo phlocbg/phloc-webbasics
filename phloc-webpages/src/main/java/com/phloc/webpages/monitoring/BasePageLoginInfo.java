3553a35e51a529e1d16c5cfa188fa8708e6779c4
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-10-14 05:56:07 +0000

    Simplified
    Former-commit-id: df98f42bfa499a8f917279b7a8c4f73abe05a352

--- phloc-webpages/src/main/java/com/phloc/webpages/monitoring/BasePageLoginInfo.java
+++ phloc-webpages/src/main/java/com/phloc/webpages/monitoring/BasePageLoginInfo.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.phloc.webpages.security;
+package com.phloc.webpages.monitoring;
 
 import java.util.Collection;
 import java.util.Locale;
