496aaf34ac7c6d2af7acfd0bcc963f7e52d40018
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-10-18 17:00:37 +0000

    Finished NamedSMTPSettings + micro type converter
    Former-commit-id: 3b0b681bc71590b9a2090e990bec61e3bcb64104

--- phloc-webbasics/src/main/java/com/phloc/webbasics/smtp/NamedSMTPSettings.java
+++ phloc-webbasics/src/main/java/com/phloc/webbasics/smtp/NamedSMTPSettings.java
@@ -3,22 +3,41 @@ package com.phloc.webbasics.smtp;
 import javax.annotation.Nonnull;
 
 import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.hash.HashCodeGenerator;
+import com.phloc.commons.id.IHasID;
+import com.phloc.commons.idfactory.GlobalIDFactory;
+import com.phloc.commons.state.EChange;
 import com.phloc.commons.string.StringHelper;
-import com.phloc.web.smtp.impl.SMTPSettings;
+import com.phloc.commons.string.ToStringGenerator;
+import com.phloc.web.smtp.ISMTPSettings;
 
-public class NamedSMTPSettings
+public class NamedSMTPSettings implements IHasID <String>
 {
-  private final String m_sName;
-  private final SMTPSettings m_aSettings;
+  private final String m_sID;
+  private String m_sName;
+  private ISMTPSettings m_aSMTPSettings;
 
-  public NamedSMTPSettings (@Nonnull @Nonempty final String sName, @Nonnull final SMTPSettings aSettings)
+  public NamedSMTPSettings (@Nonnull @Nonempty final String sName, @Nonnull final ISMTPSettings aSMTPSettings)
   {
-    if (StringHelper.hasNoText (sName))
-      throw new IllegalArgumentException ("name");
-    if (aSettings == null)
-      throw new NullPointerException ("settings");
-    m_sName = sName;
-    m_aSettings = aSettings;
+    this (GlobalIDFactory.getNewPersistentStringID (), sName, aSMTPSettings);
+  }
+
+  NamedSMTPSettings (@Nonnull @Nonempty final String sID,
+                     @Nonnull @Nonempty final String sName,
+                     @Nonnull final ISMTPSettings aSMTPSettings)
+  {
+    if (StringHelper.hasNoText (sID))
+      throw new IllegalArgumentException ("ID");
+    m_sID = sID;
+    setName (sName);
+    setSMTPSettings (aSMTPSettings);
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getID ()
+  {
+    return m_sID;
   }
 
   @Nonnull
@@ -29,8 +48,56 @@ public class NamedSMTPSettings
   }
 
   @Nonnull
-  public SMTPSettings getSettins ()
+  public EChange setName (@Nonnull @Nonempty final String sName)
   {
-    return m_aSettings;
+    if (StringHelper.hasNoText (sName))
+      throw new IllegalArgumentException ("name");
+    if (sName.equals (m_sName))
+      return EChange.UNCHANGED;
+    m_sName = sName;
+    return EChange.CHANGED;
+  }
+
+  @Nonnull
+  public ISMTPSettings getSMTPSettings ()
+  {
+    return m_aSMTPSettings;
+  }
+
+  @Nonnull
+  public EChange setSMTPSettings (@Nonnull final ISMTPSettings aSMTPSettings)
+  {
+    if (aSMTPSettings == null)
+      throw new NullPointerException ("SMTPSettings");
+    if (aSMTPSettings.equals (m_aSMTPSettings))
+      return EChange.UNCHANGED;
+    m_aSMTPSettings = aSMTPSettings;
+    return EChange.CHANGED;
+  }
+
+  @Override
+  public boolean equals (final Object o)
+  {
+    if (o == this)
+      return true;
+    if (o == null || !getClass ().equals (o.getClass ()))
+      return false;
+    final NamedSMTPSettings rhs = (NamedSMTPSettings) o;
+    return m_sID.equals (rhs.m_sID);
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    return new HashCodeGenerator (this).append (m_sID).getHashCode ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return new ToStringGenerator (this).append ("ID", m_sID)
+                                       .append ("name", m_sName)
+                                       .append ("SMTPsettings", m_aSMTPSettings)
+                                       .toString ();
   }
 }
