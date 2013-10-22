596eabfcb9d4f146ebee0138a7228942daf5325f
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-10-22 07:38:37 +0000

    API cleansing and comments added
    Former-commit-id: a634f96c66cabe1aaed828aaff86c7ed6a0dd435

--- phloc-webbasics/src/main/java/com/phloc/webbasics/smtp/NamedSMTPSettingsManager.java
+++ phloc-webbasics/src/main/java/com/phloc/webbasics/smtp/NamedSMTPSettingsManager.java
@@ -19,12 +19,12 @@ import com.phloc.commons.microdom.IMicroElement;
 import com.phloc.commons.microdom.convert.MicroTypeConverter;
 import com.phloc.commons.microdom.impl.MicroDocument;
 import com.phloc.commons.state.EChange;
+import com.phloc.commons.string.StringHelper;
 import com.phloc.commons.string.ToStringGenerator;
-import com.phloc.web.smtp.impl.SMTPSettings;
+import com.phloc.web.smtp.ISMTPSettings;
 
 /**
- * This class contains all globally available SMTP settings. The settings are
- * identified by name.
+ * This class manages {@link NamedSMTPSettings} objects.
  * 
  * @author Philip Helger
  */
@@ -98,6 +98,10 @@ public class NamedSMTPSettingsManager extends AbstractSimpleDAO implements IHasS
     }
   }
 
+  /**
+   * @return A copy of all contained SMTP settings as map from ID to object.
+   *         Never <code>null</code> but maybe empty.
+   */
   @Nonnull
   @ReturnsMutableCopy
   public Map <String, NamedSMTPSettings> getAllSettings ()
@@ -113,8 +117,18 @@ public class NamedSMTPSettingsManager extends AbstractSimpleDAO implements IHasS
     }
   }
 
+  /**
+   * Check if the specified settings ID is contained or not.
+   * 
+   * @param sID
+   *        The ID to check. May be <code>null</code>.
+   * @return <code>true</code> if the passed ID is contained
+   */
   public boolean containsSettings (@Nullable final String sID)
   {
+    if (StringHelper.hasNoText (sID))
+      return false;
+
     m_aRWLock.readLock ().lock ();
     try
     {
@@ -126,9 +140,19 @@ public class NamedSMTPSettingsManager extends AbstractSimpleDAO implements IHasS
     }
   }
 
+  /**
+   * Get the settings of the specified ID
+   * 
+   * @param sID
+   *        The ID to search. May be <code>null</code>.
+   * @return <code>null</code> if no such settings are contained.
+   */
   @Nullable
   public NamedSMTPSettings getSettings (@Nullable final String sID)
   {
+    if (StringHelper.hasNoText (sID))
+      return null;
+
     m_aRWLock.readLock ().lock ();
     try
     {
@@ -140,8 +164,19 @@ public class NamedSMTPSettingsManager extends AbstractSimpleDAO implements IHasS
     }
   }
 
-  @Nullable
-  public NamedSMTPSettings addSettings (@Nonnull @Nonempty final String sName, @Nonnull final SMTPSettings aSettings)
+  /**
+   * Create a new settings object.
+   * 
+   * @param sName
+   *        The name of the settings. May neither be <code>null</code> nor
+   *        empty.
+   * @param aSettings
+   *        The main SMTP settings. May not be <code>null</code>.
+   * @return The created {@link NamedSMTPSettings} object and never
+   *         <code>null</code>.
+   */
+  @Nonnull
+  public NamedSMTPSettings addSettings (@Nonnull @Nonempty final String sName, @Nonnull final ISMTPSettings aSettings)
   {
     final NamedSMTPSettings aNamedSettings = new NamedSMTPSettings (sName, aSettings);
 
@@ -149,6 +184,7 @@ public class NamedSMTPSettingsManager extends AbstractSimpleDAO implements IHasS
     try
     {
       _addItem (aNamedSettings);
+      markAsChanged ();
       return aNamedSettings;
     }
     finally
@@ -157,13 +193,85 @@ public class NamedSMTPSettingsManager extends AbstractSimpleDAO implements IHasS
     }
   }
 
+  /**
+   * Update an existing settings object.
+   * 
+   * @param sID
+   *        The ID of the settings object to be updated. May be
+   *        <code>null</code>.
+   * @param sName
+   *        The new name of the settings. May neither be <code>null</code> nor
+   *        empty.
+   * @param aSettings
+   *        The new SMTP settings. May not be <code>null</code>.
+   * @return {@link EChange#CHANGED} if something was changed.
+   */
+  @Nullable
+  public EChange updateSettings (@Nullable final String sID,
+                                 @Nonnull @Nonempty final String sName,
+                                 @Nonnull final ISMTPSettings aSettings)
+  {
+    final NamedSMTPSettings aNamedSettings = getSettings (sID);
+    if (aNamedSettings == null)
+      return EChange.UNCHANGED;
+
+    m_aRWLock.writeLock ().lock ();
+    try
+    {
+      EChange eChange = EChange.UNCHANGED;
+      eChange = eChange.or (aNamedSettings.setName (sName));
+      eChange = eChange.or (aNamedSettings.setSMTPSettings (aSettings));
+      if (eChange.isChanged ())
+        markAsChanged ();
+      return eChange;
+    }
+    finally
+    {
+      m_aRWLock.writeLock ().unlock ();
+    }
+  }
+
+  /**
+   * Remove the SMTP settings with the specified ID.
+   * 
+   * @param sID
+   *        The ID to be removed. May be <code>null</code>.
+   * @return {@link EChange#CHANGED} if a removal was performed.
+   */
   @Nullable
   public EChange removeSettings (@Nullable final String sID)
   {
     m_aRWLock.writeLock ().lock ();
     try
     {
-      return EChange.valueOf (m_aMap.remove (sID) != null);
+      if (m_aMap.remove (sID) == null)
+        return EChange.UNCHANGED;
+      markAsChanged ();
+      return EChange.CHANGED;
+    }
+    finally
+    {
+      m_aRWLock.writeLock ().unlock ();
+    }
+  }
+
+  /**
+   * Remove all contained SMTP settings
+   * 
+   * @return {@link EChange#CHANGED} if a removal was performed.
+   */
+  @Nullable
+  public EChange removeAllSettings ()
+  {
+    m_aRWLock.writeLock ().lock ();
+    try
+    {
+      if (m_aMap.isEmpty ())
+        return EChange.UNCHANGED;
+
+      m_aMap.clear ();
+      markAsChanged ();
+      return EChange.CHANGED;
     }
     finally
     {
