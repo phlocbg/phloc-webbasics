a16c48acaa4806f375fd40c44fda0852091de01d
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-10-23 08:43:12 +0000

    Updated to phloc-web 5.3.1
    Former-commit-id: 947d91b4315bba92bca6d0b2971e09c119684709

--- phloc-webpages/src/main/java/com/phloc/webpages/sysinfo/BasePageSysInfoNetwork.java
+++ phloc-webpages/src/main/java/com/phloc/webpages/sysinfo/BasePageSysInfoNetwork.java
@@ -18,7 +18,7 @@
 package com.phloc.webpages.sysinfo;
 
 import java.net.NetworkInterface;
-import java.util.List;
+import java.net.SocketException;
 import java.util.Locale;
 
 import javax.annotation.Nonnull;
@@ -27,24 +27,32 @@ import javax.annotation.Nullable;
 import com.phloc.commons.GlobalDebug;
 import com.phloc.commons.annotations.Nonempty;
 import com.phloc.commons.annotations.Translatable;
-import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.commons.compare.ESortOrder;
+import com.phloc.commons.hierarchy.DefaultHierarchyWalkerCallback;
 import com.phloc.commons.name.IHasDisplayText;
 import com.phloc.commons.string.StringHelper;
 import com.phloc.commons.text.IReadonlyMultiLingualText;
 import com.phloc.commons.text.ITextProvider;
 import com.phloc.commons.text.impl.TextProvider;
 import com.phloc.commons.text.resolve.DefaultTextResolver;
+import com.phloc.commons.tree.utils.sort.TreeWithIDSorter;
+import com.phloc.commons.tree.utils.walk.TreeWalker;
+import com.phloc.commons.tree.withid.DefaultTreeItemWithID;
+import com.phloc.commons.tree.withid.unique.DefaultTreeWithGlobalUniqueID;
 import com.phloc.css.ECSSUnit;
 import com.phloc.css.property.CCSSProperties;
 import com.phloc.html.hc.IHCCell;
 import com.phloc.html.hc.html.HCCol;
+import com.phloc.html.hc.html.HCEM;
 import com.phloc.html.hc.html.HCRow;
 import com.phloc.html.hc.impl.HCNodeList;
 import com.phloc.web.networkinterface.ComparatorNetworkInterfaceName;
+import com.phloc.web.networkinterface.NetworkInterfaceUtils;
 import com.phloc.webbasics.EWebBasicsText;
 import com.phloc.webbasics.app.page.WebPageExecutionContext;
 import com.phloc.webctrls.custom.tabbox.ITabBox;
 import com.phloc.webctrls.custom.table.IHCTableFormView;
+import com.phloc.webctrls.datatables.DataTables;
 import com.phloc.webpages.AbstractWebPageExt;
 import com.phloc.webpages.EWebPageText;
 
@@ -60,7 +68,6 @@ public class BasePageSysInfoNetwork extends AbstractWebPageExt
   {
     MSG_NETWORK_INTERFACES ("Netzwerkkarten", "Network interfaces"),
     MSG_ERROR_FINDING ("Fehler beim Ermitteln der Netzwerkkarten", "Error determining network interfaces"),
-    MSG_PROXY ("Proxy", "Proxy"),
     MSG_ID ("ID", "id"),
     MSG_NAME ("Name", "Name"),
     MSG_MAC ("MAC Adresse", "MAC address"),
@@ -69,7 +76,8 @@ public class BasePageSysInfoNetwork extends AbstractWebPageExt
     MSG_IS_POINT_TO_POINT ("P2P?", "P2P?"),
     MSG_IS_MULTICAST ("MC?", "MC?"),
     MSG_MTU ("MTU", "MTU"),
-    MSG_IS_VIRTUAL ("Virt?", "Virt?");
+    MSG_IS_VIRTUAL ("Virt?", "Virt?"),
+    MSG_ERROR ("Fehler!", "Error!");
 
     private final ITextProvider m_aTP;
 
@@ -128,7 +136,7 @@ public class BasePageSysInfoNetwork extends AbstractWebPageExt
                                                                             new HCCol (40),
                                                                             new HCCol (40),
                                                                             new HCCol (40));
-      aTable.setID (getID () + "$ni");
+      aTable.setID (getID () + "-ni");
       aTable.addHeaderRow ().addCells (EText.MSG_ID.getDisplayText (aDisplayLocale),
                                        EText.MSG_NAME.getDisplayText (aDisplayLocale),
                                        EText.MSG_MAC.getDisplayText (aDisplayLocale),
@@ -140,50 +148,120 @@ public class BasePageSysInfoNetwork extends AbstractWebPageExt
                                        EText.MSG_IS_VIRTUAL.getDisplayText (aDisplayLocale));
       try
       {
-        final List <NetworkInterface> aNIs = ContainerHelper.newList (NetworkInterface.getNetworkInterfaces ());
-        ContainerHelper.getSortedInline (aNIs, new ComparatorNetworkInterfaceName ());
-        for (final NetworkInterface aNI : aNIs)
-        {
-          int nDepth = 0;
-          NetworkInterface aCur = aNI;
-          while (aCur.getParent () != null)
-          {
-            nDepth++;
-            aCur = aCur.getParent ();
-          }
+        final DefaultTreeWithGlobalUniqueID <String, NetworkInterface> aNITree = NetworkInterfaceUtils.createNetworkInterfaceTree ();
+        // Sort on each level
+        TreeWithIDSorter.sortByValue (aNITree, new ComparatorNetworkInterfaceName ());
+        TreeWalker.walkTree (aNITree,
+                             new DefaultHierarchyWalkerCallback <DefaultTreeItemWithID <String, NetworkInterface>> ()
+                             {
+                               @Override
+                               public void onItemBeforeChildren (@Nonnull final DefaultTreeItemWithID <String, NetworkInterface> aItem)
+                               {
+                                 final NetworkInterface aNI = aItem.getData ();
+                                 final int nDepth = getLevel ();
+                                 final HCRow aRow = aTable.addBodyRow ();
+                                 aRow.addCell (aNI.getName ());
+                                 final IHCCell <?> aCell = aRow.addAndReturnCell (aNI.getDisplayName ());
+                                 if (nDepth > 0)
+                                   aCell.addStyle (CCSSProperties.PADDING_LEFT.newValue (ECSSUnit.em (nDepth)));
 
-          final HCRow aRow = aTable.addBodyRow ();
-          aRow.addCell (aNI.getName ());
-          final IHCCell <?> aCell = aRow.addAndReturnCell (aNI.getDisplayName ());
-          if (nDepth > 0)
-            aCell.addStyle (CCSSProperties.PADDING_LEFT.newValue (ECSSUnit.em (nDepth)));
+                                 // hardware address (usually MAC) of the
+                                 // interface if it has one and if it can be
+                                 // accessed given the current privileges.
+                                 try
+                                 {
+                                   final byte [] aMAC = aNI.getHardwareAddress ();
+                                   aRow.addCell (aMAC == null ? "" : StringHelper.getHexEncoded (aMAC));
+                                 }
+                                 catch (final SocketException ex)
+                                 {
+                                   aRow.addCell (HCEM.create (EText.MSG_ERROR.getDisplayText (aDisplayLocale)));
+                                 }
 
-          final byte [] aMAC = aNI.getHardwareAddress ();
-          aRow.addCell (aMAC == null ? "" : StringHelper.getHexEncoded (aMAC));
+                                 // network interface is up and running.
+                                 try
+                                 {
+                                   aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isUp (), aDisplayLocale));
+                                 }
+                                 catch (final SocketException ex)
+                                 {
+                                   aRow.addCell (HCEM.create (EText.MSG_ERROR.getDisplayText (aDisplayLocale)));
+                                 }
 
-          aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isUp (), aDisplayLocale));
+                                 // network interface is a loopback interface.
+                                 try
+                                 {
+                                   aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isLoopback (), aDisplayLocale));
+                                 }
+                                 catch (final SocketException ex)
+                                 {
+                                   aRow.addCell (HCEM.create (EText.MSG_ERROR.getDisplayText (aDisplayLocale)));
+                                 }
 
-          aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isLoopback (), aDisplayLocale));
+                                 // network interface is a point to point
+                                 // interface. A typical point to point
+                                 // interface would be a PPP connection through
+                                 // a modem.
+                                 try
+                                 {
+                                   aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isPointToPoint (), aDisplayLocale));
+                                 }
+                                 catch (final SocketException ex)
+                                 {
+                                   aRow.addCell (HCEM.create (EText.MSG_ERROR.getDisplayText (aDisplayLocale)));
+                                 }
 
-          aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isPointToPoint (), aDisplayLocale));
+                                 // network interface supports multicasting or
+                                 // not.
+                                 try
+                                 {
+                                   aRow.addCell (EWebBasicsText.getYesOrNo (aNI.supportsMulticast (), aDisplayLocale));
+                                 }
+                                 catch (final SocketException ex)
+                                 {
+                                   aRow.addCell (HCEM.create (EText.MSG_ERROR.getDisplayText (aDisplayLocale)));
+                                 }
 
-          aRow.addCell (EWebBasicsText.getYesOrNo (aNI.supportsMulticast (), aDisplayLocale));
+                                 // Maximum Transmission Unit (MTU) of this
+                                 // interface.
+                                 int nMTU = -1;
+                                 try
+                                 {
+                                   nMTU = aNI.getMTU ();
+                                 }
+                                 catch (final SocketException ex)
+                                 {}
+                                 if (nMTU > 0)
+                                   aRow.addCell (Integer.toString (nMTU));
+                                 else
+                                   aRow.addCell ();
 
-          final int nMTU = aNI.getMTU ();
-          if (nMTU > 0)
-            aRow.addCell (Integer.toString (nMTU));
-          else
-            aRow.addCell ();
-
-          aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isVirtual (), aDisplayLocale));
-        }
+                                 // this interface is a virtual interface (also
+                                 // called subinterface). Virtual interfaces
+                                 // are, on some systems, interfaces created as
+                                 // a child of a physical interface and given
+                                 // different settings (like address or MTU).
+                                 // Usually the name of the interface will the
+                                 // name of the parent followed by a colon (:)
+                                 // and a number identifying the child since
+                                 // there can be several virtual interfaces
+                                 // attached to a single physical interface
+                                 aRow.addCell (EWebBasicsText.getYesOrNo (aNI.isVirtual (), aDisplayLocale));
+                               }
+                             });
       }
-      catch (final Exception ex)
+      catch (final Throwable t)
       {
         aTable.addSpanningBodyContent (EText.MSG_ERROR_FINDING.getDisplayText (aDisplayLocale) +
-                                       (GlobalDebug.isDebugMode () ? ": " + ex.getMessage () : ""));
+                                       (GlobalDebug.isDebugMode () ? ": " + t.getMessage () : ""));
       }
-      aTabBox.addTab (EText.MSG_NETWORK_INTERFACES.getDisplayText (aDisplayLocale), aTable);
+
+      final DataTables aDataTables = getStyler ().createDefaultDataTables (aTable, aDisplayLocale);
+      aDataTables.setDisplayLength (-1);
+      aDataTables.setInitialSorting (0, ESortOrder.ASCENDING);
+
+      aTabBox.addTab (EText.MSG_NETWORK_INTERFACES.getDisplayText (aDisplayLocale),
+                      new HCNodeList ().addChild (aTable).addChild (aDataTables));
     }
     aNodeList.addChild (aTabBox);
   }
