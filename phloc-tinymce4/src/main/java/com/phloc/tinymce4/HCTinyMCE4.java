912f2631c0b4be3b6bc21ffe83a3ebd8d8ac360e
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2013-10-28 08:37:42 +0000

    Former-commit-id: 9bc2630a9129bcd1827124c48962be2d4d1244a8

--- phloc-tinymce4/src/main/java/com/phloc/tinymce4/HCTinyMCE4.java
+++ phloc-tinymce4/src/main/java/com/phloc/tinymce4/HCTinyMCE4.java
@@ -18,18 +18,103 @@
 package com.phloc.tinymce4;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.state.ETriState;
 import com.phloc.commons.string.StringHelper;
 import com.phloc.html.hc.IHCNodeBuilder;
+import com.phloc.html.hc.api.EHCTextDirection;
 import com.phloc.html.hc.html.HCScript;
 import com.phloc.html.js.builder.JSAssocArray;
 
+/**
+ * Wraps TinyMCE4 into an HC node. The only required settings is
+ * {@link #setSelector(String)} but this does not need to be called, as the
+ * default value {@link #DEFAULT_SELECTOR} is used automatically.
+ * 
+ * @author Philip Helger
+ */
 public class HCTinyMCE4 implements IHCNodeBuilder
 {
+  public static final boolean DEFAULT_BROWSER_SPELLCHECK = false;
   public static final String DEFAULT_SELECTOR = "textarea";
 
+  private String m_sAutoFocus;
+  private ETriState m_eBrowserSpellcheck = ETriState.UNDEFINED;
+  private EHCTextDirection m_eDirectionality;
   private String m_sSelector = DEFAULT_SELECTOR;
+
+  @Nullable
+  public String getAutoFocus ()
+  {
+    return m_sAutoFocus;
+  }
+
+  /**
+   * This option enables you to auto focus an editor instance. The value of this
+   * option should be an editor instance id. The editor instance id is the id
+   * for the original textarea or div element that got replaced.
+   * 
+   * @param sAutoFocus
+   *        Editor ID
+   */
+  public void setAutoFocus (@Nullable final String sAutoFocus)
+  {
+    m_sAutoFocus = sAutoFocus;
+  }
+
+  public boolean isBrowserSpellcheck ()
+  {
+    return m_eBrowserSpellcheck.getAsBooleanValue (DEFAULT_BROWSER_SPELLCHECK);
+  }
+
+  /**
+   * This is a true/false value if the usage of the browsers internal
+   * spellchecker should be used. Default value is false.
+   * 
+   * @param bBrowserSpellcheck
+   *        <code>true</code> to enabled, <code>false</code> to disable
+   */
+  public void setBrowserSpellcheck (final boolean bBrowserSpellcheck)
+  {
+    m_eBrowserSpellcheck = ETriState.valueOf (bBrowserSpellcheck);
+  }
+
+  /**
+   * This is a true/false value if the usage of the browsers internal
+   * spellchecker should be used. Default value is false.
+   * 
+   * @param aBrowserSpellcheck
+   *        <code>true</code> to enabled, <code>false</code> to disable and
+   *        <code>null</code> for default value.
+   */
+  public void setBrowserSpellcheck (@Nullable final Boolean aBrowserSpellcheck)
+  {
+    m_eBrowserSpellcheck = ETriState.valueOf (aBrowserSpellcheck);
+  }
+
+  @Nullable
+  public EHCTextDirection getDirectionality ()
+  {
+    return m_eDirectionality;
+  }
+
+  /**
+   * <pre>
+   * Set the default directionality of the editor.
+   * Possible values are:
+   * - ltr
+   *  - rtl
+   * </pre>
+   * 
+   * @param eDirectionality
+   *        direction
+   */
+  public void setDirectionality (@Nullable final EHCTextDirection eDirectionality)
+  {
+    m_eDirectionality = eDirectionality;
+  }
 
   @Nonnull
   @Nonempty
@@ -52,6 +137,7 @@ public class HCTinyMCE4 implements IHCNodeBuilder
    * </pre>
    * 
    * @param sSelector
+   *        selector
    */
   public void setSelector (@Nonnull @Nonempty final String sSelector)
   {
@@ -64,6 +150,12 @@ public class HCTinyMCE4 implements IHCNodeBuilder
   public HCScript build ()
   {
     final JSAssocArray aOptions = new JSAssocArray ();
+    if (StringHelper.hasText (m_sAutoFocus))
+      aOptions.add ("auto_focus", m_sAutoFocus);
+    if (m_eBrowserSpellcheck.isDefined ())
+      aOptions.add ("browser_spellcheck", isBrowserSpellcheck ());
+    if (m_eDirectionality != null)
+      aOptions.add ("directionality", m_eDirectionality.getAttrValue ());
     aOptions.add ("selector", m_sSelector);
     return new HCScript (JSTinyMCE4.init (aOptions));
   }
