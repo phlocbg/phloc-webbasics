305912b275060a1c7c03ce34202e69cc08ae8f6b
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2014-04-03 12:21:32 +0000

    Extended API to use an IReadonlyMultilingualText as well
    Former-commit-id: fde37e5aa781174d7bd5ad0783eb3b790a4344af

--- phloc-webpages/src/main/java/com/phloc/webpages/PageViewExternal.java
+++ phloc-webpages/src/main/java/com/phloc/webpages/PageViewExternal.java
@@ -19,6 +19,7 @@ package com.phloc.webpages;
 
 import javax.annotation.Nonnull;
 
+import com.phloc.commons.ValueEnforcer;
 import com.phloc.commons.annotations.Nonempty;
 import com.phloc.commons.charset.CCharset;
 import com.phloc.commons.io.IReadableResource;
@@ -27,51 +28,93 @@ import com.phloc.commons.microdom.IMicroDocument;
 import com.phloc.commons.microdom.IMicroElement;
 import com.phloc.commons.microdom.IMicroNode;
 import com.phloc.commons.microdom.serialize.MicroReader;
+import com.phloc.commons.text.IReadonlyMultiLingualText;
 import com.phloc.commons.xml.serialize.SAXReaderSettings;
 import com.phloc.html.entities.HTMLEntityResolver;
-import com.phloc.html.hc.conversion.HCSettings;
 import com.phloc.html.hc.impl.HCDOMWrapper;
+import com.phloc.webbasics.app.html.WebHTMLCreator;
 import com.phloc.webbasics.app.page.WebPageExecutionContext;
 
 /**
  * Renders a page with HTML code that is provided from an external resource
  * (e.g. for static pages)
- * 
+ *
  * @author Philip Helger
  */
 public class PageViewExternal extends AbstractWebPageExt
 {
+  private final IReadableResource m_aRes;
+  private boolean m_bReadEveryTime = false;
   private final IMicroElement m_aDocElem;
 
-  public PageViewExternal (@Nonnull @Nonempty final String sID,
-                           @Nonnull final String sName,
-                           @Nonnull final IReadableResource aRes)
+  @Nonnull
+  private static IMicroElement _readPage (@Nonnull final IReadableResource aResource)
   {
-    super (sID, sName);
-
     // Read content once
-    final String sContent = StreamUtils.getAllBytesAsString (aRes, CCharset.CHARSET_UTF_8_OBJ);
+    final String sContent = StreamUtils.getAllBytesAsString (aResource, CCharset.CHARSET_UTF_8_OBJ);
     if (sContent == null)
-      throw new IllegalStateException ("Failed to read " + aRes.toString ());
+      throw new IllegalStateException ("Failed to read " + aResource.toString ());
 
     // Parse content once
     final String sParsable = "<x xmlns='" +
-                             HCSettings.getConversionSettings (false).getHTMLVersion ().getNamespaceURI () +
+                             WebHTMLCreator.getHTMLVersion ().getNamespaceURI () +
                              "'>" +
                              sContent +
                              "</x>";
     final IMicroDocument aDoc = MicroReader.readMicroXML (sParsable,
                                                           new SAXReaderSettings ().setEntityResolver (HTMLEntityResolver.getInstance ()));
     if (aDoc == null || aDoc.getDocumentElement () == null)
-      throw new IllegalStateException ("Failed to parse code for page " + aRes.toString ());
-    m_aDocElem = aDoc.getDocumentElement ();
+      throw new IllegalStateException ("Failed to parse code for page " + aResource.toString ());
+    return aDoc.getDocumentElement ();
+  }
+
+  public PageViewExternal (@Nonnull @Nonempty final String sID,
+                           @Nonnull final String sName,
+                           @Nonnull final IReadableResource aResource)
+  {
+    super (sID, sName);
+    m_aRes = ValueEnforcer.notNull (aResource, "Resource");
+
+    // Read once anyway to check if the resource is readable
+    m_aDocElem = _readPage (aResource);
+  }
+
+  public PageViewExternal (@Nonnull @Nonempty final String sID,
+                           @Nonnull final IReadonlyMultiLingualText aName,
+                           @Nonnull final IReadableResource aResource)
+  {
+    super (sID, aName);
+    m_aRes = ValueEnforcer.notNull (aResource, "Resource");
+
+    // Read once anyway to check if the resource is readable
+    m_aDocElem = _readPage (aResource);
+  }
+
+  @Nonnull
+  public IReadableResource getResource ()
+  {
+    return m_aRes;
+  }
+
+  public boolean isReadEveryTime ()
+  {
+    return m_bReadEveryTime;
+  }
+
+  @Nonnull
+  public PageViewExternal setReadEveryTime (final boolean bReadEveryTime)
+  {
+    m_bReadEveryTime = bReadEveryTime;
+    return this;
   }
 
   @Override
   protected void fillContent (@Nonnull final WebPageExecutionContext aWPEC)
   {
-    if (m_aDocElem.hasChildren ())
-      for (final IMicroNode aChild : m_aDocElem.getChildren ())
-        aWPEC.getNodeList ().addChild (new HCDOMWrapper (aChild.getClone ()));
+    final IMicroElement aElement = m_bReadEveryTime ? _readPage (m_aRes) : m_aDocElem;
+
+    if (aElement.hasChildren ())
+      for (final IMicroNode aChild : aElement.getChildren ())
+        aWPEC.getNodeList ().addChild (new HCDOMWrapper (aChild));
   }
 }
