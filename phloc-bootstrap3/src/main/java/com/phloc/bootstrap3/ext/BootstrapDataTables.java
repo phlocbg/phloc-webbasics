cc29c47ce3f670606de08531dbcb60f65c0bed60
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2014-07-01 15:09:55 +0000

    Former-commit-id: 7944c18aabc8caefb09d232e967b1f1582897890

--- phloc-bootstrap3/src/main/java/com/phloc/bootstrap3/ext/BootstrapDataTables.java
+++ phloc-bootstrap3/src/main/java/com/phloc/bootstrap3/ext/BootstrapDataTables.java
@@ -29,7 +29,7 @@ import com.phloc.webctrls.datatables.DataTables;
 
 public class BootstrapDataTables extends DataTables
 {
-  public static final String DEFAULT_DOM = "<'row'<'col-xs-6'l><'col-xs-6'f>r>t<'row'<'col-xs-6'i><'col-xs-6'p>>";
+  public static final String DEFAULT_DOM = "<'row'<'col-xs-6'l><'col-xs-6'f>r>t<'row'<'col-xs-5'i><'col-xs-7'p>>";
 
   @SuppressWarnings ("deprecation")
   public BootstrapDataTables (@Nonnull final IHCTable <?> aTable)
