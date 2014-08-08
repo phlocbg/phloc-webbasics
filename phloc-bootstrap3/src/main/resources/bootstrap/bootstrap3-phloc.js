a4512b18fa82327a6cef611eb10c37521da7af23
ph@phloc.com <ph@phloc.com@224af017-8982-1a56-601f-39e8d6e75726>
2014-08-08 07:53:33 +0000

    Former-commit-id: 7a35afd351e177c7ad09004ea4e2d0315a1dd273

--- phloc-bootstrap3/src/main/resources/bootstrap/bootstrap3-phloc.js
+++ phloc-bootstrap3/src/main/resources/bootstrap/bootstrap3-phloc.js
@@ -10,5 +10,13 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
       "@-ms-viewport{width:auto!important}"
     )
   )
-  document.getElementsByTagName("head")[0].appendChild(msViewportStyle)
+  document.querySelector('head').appendChild(msViewportStyle);
 }
+
+$(function () {
+  var nua = navigator.userAgent;
+  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1);
+  if (isAndroid) {
+    $('select.form-control').removeClass('form-control').css('width', '100%');
+  }
+})
